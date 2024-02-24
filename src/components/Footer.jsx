a49d1f8b703b0b40711eb83e831a5d496c09858e
mdraza <raza.uidev@gmail.com>
2024-02-24 13:07:48 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="w-100 py-3 bg-[#150332] dark:bg-[#121212] dark:border-t-2 border-[#2b2b2b] text-slate-200 text-center mt-8 text-sm">
+      Designed & Developed by Md Rajaullah Ansari (@mdrajaullah)
+    </div>
+  );
+};
+
+export default Footer;
