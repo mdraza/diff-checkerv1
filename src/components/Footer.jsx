d23106fe591708e7cdf933b68db497ce085154f9
mdraza <raza.uidev@gmail.com>
2024-03-05 08:41:37 +0530

    footer text changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="w-100 py-3 bg-[#150332] dark:bg-[#121212] dark:border-t-2 border-[#2b2b2b] text-slate-200 text-center mt-[40px] md:mt-[65px] text-sm">
-      Designed & Developed by Md Rajaullah Ansari (@mdrajaullah)
+      Designed & Deployed by <b>Md Rajaullah Ansari</b> (mdrajaullah@)
     </div>
   );
 };
